c4c99d4488f33c62a2700c357f868df207df78ff
Beatriz-Ramerindo <janeramerindo@gmail.com>
2021-10-03 23:43:04 -0300

    update: all depedencies , add:   Pages/recorvy-passoword, a temporary path for development mode in Content

--- portalcodivasapp/src/Components/Content.jsx
+++ portalcodivasapp/src/Components/Content.jsx
@@ -1,5 +1,7 @@
 import { Switch, Route } from "react-router-dom";
 
+
+import RecorvyPassword from "../Pages/recorvy-password/recorvy-passoword";
 const Content = () => {
     return (
         <main className="main-content">
@@ -11,6 +13,10 @@ const Content = () => {
                 <Route path="/projects">
                     <h2>PROJECTS</h2>
                     {/* IMPORT & INSERT PAGE CONTENT HERE */}
+                </Route>
+                <Route path="/recorvy-password">
+                    { /*this is just an adaptation for development mode. */ }
+                    { process.env.NODE_ENV === 'development' && <RecorvyPassword /> }
                 </Route>
             </Switch>
         </main>
---
+++ portalcodivasapp/src/Pages/recorvy-password/recorvy-passoword.jsx
@@ -0,0 +1,7 @@
+const RecorvyPassword = () => (
+    <div>
+        <h1>Pag</h1>
+    </div>
+)
+
+export default RecorvyPassword
